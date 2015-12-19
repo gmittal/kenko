94e21fc0de3e1519daf95bde08562f3c7c43ddce
gmittal <gautam@mittal.net>
2015-12-19 15:57:49 -0800

    updates

--- server/nutritionize/nutritionize.js
+++ server/nutritionize/nutritionize.js
@@ -10,13 +10,17 @@ require('shelljs/global');
 
 var searchQuery = process.argv[2];
 if (searchQuery) {
-    var netResult = JSON.parse(exec('nutritionize/./phantomjs nutritionize/deps/nutritionize-net.js "'+ searchQuery +'"').output);
+    var netResult = JSON.parse(exec('nutritionize/./phantomjs nutritionize/deps/nutritionize-net.js "'+ searchQuery +'"', {silent:true}).output);
 
     var png = new img(netResult.rawImage_path);
 
     png.crop({x:57, y:184, height: netResult.size.height+22, width: netResult.size.width+22}, function (err, image) {
 	fs.writeFile(netResult.rawImage_path, image.data, function (err) {
 	    if (err) console.log(err);
+	    var uploaded_image = exec('curl -F "file=@'+netResult.rawImage_path+'" https://file.io', {silent:true}).output.split("\n");
+	    console.log(JSON.parse(uploaded_image[uploaded_image.length-1]).link);
+	    rm('-rf', netResult.rawImage_path); // delete what is now stored in the cloud
+	    process.exit();
 	});
     });
 
