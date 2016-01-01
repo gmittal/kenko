3b42dc3fc6d4a998d197a95a53cfeea6d7d2ec02
gmittal <gautam@mittal.net>
2016-01-01 12:02:52 -0800

    more updates

--- server/nutritionize/nutritionize.js
+++ server/nutritionize/nutritionize.js
@@ -23,7 +23,7 @@ app.use(function(req, res, next) { // enable CORS
 // Send search query, receive awesomeness back
 app.post('/nutritionize', function (req, res) {
   if (req.body.query) {
-        var netResult = JSON.parse(exec('./phantomjs nutritionize/deps/nutritionize-net.js "'+ searchQuery +'"', {silent:true}).output);
+        var netResult = JSON.parse(exec('./phantomjs deps/nutritionize-net.js "'+ searchQuery +'"', {silent:true}).output);
 
         var png = new img(netResult.rawImage_path);
 
