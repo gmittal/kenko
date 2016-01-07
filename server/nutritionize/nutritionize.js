3ba79fd74637c05f69727a73da391f07abe1a4df
Gautam Mittal <gautam@mittal.net>
2016-01-07 02:16:33 +0000

    fixed minor bug in nutritionize.js regarding label size

--- server/nutritionize/nutritionize.js
+++ server/nutritionize/nutritionize.js
@@ -43,8 +43,8 @@ app.post('/nutritionize', function (req, res) {
               rm('-rf', photo.rawImage_path); // delete what is now stored in the cloud
               console.log(JSON.parse(uploaded_image[uploaded_image.length-1]).link);
               var finalFinish = JSON.parse(netResult);
-		finalFinish["LABEL_HEIGHT"] = photo.height;
-		finalFinish["LABEL_WIDTH"] = photo.width;
+		finalFinish["LABEL_HEIGHT"] = photo.size.height;
+		finalFinish["LABEL_WIDTH"] = photo.size.width;
               finalFinish["NUTRITION_LABEL"] = JSON.parse(uploaded_image[uploaded_image.length-1]).link;
               res.send(finalFinish);
             });
